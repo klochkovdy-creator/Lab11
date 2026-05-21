ca5b5e4cf9b59f9c94d122bb246fbe9eb51de94d
klochkovdy-creator <klochkovdy@mer.ci.nsu.ru>
2026-05-21 11:55:20 +0700

    Update Contact.cs

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -1,7 +1,8 @@
-﻿namespace PhoneBook.Models
+namespace PhoneBook.Models
 {
     public class Contact
     {
+        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
     }
