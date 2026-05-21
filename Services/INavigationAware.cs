5294f5c0c609423aa4009e31dd0353c81fbda8d6
klochkovdy-creator <klochkovdy@mer.ci.nsu.ru>
2026-05-21 11:58:14 +0700

    Update INavigationAware.cs

--- Services/INavigationAware.cs
+++ Services/INavigationAware.cs
@@ -1,8 +1,5 @@
 ﻿namespace PhoneBook.Services
 {
-    // Интерфейс для ViewModel, которым нужно получать параметры при навигации.
-    // NavigationService вызывает OnNavigatedTo до установки CurrentViewModel,
-    // чтобы данные были готовы к моменту отображения View.
     public interface INavigationAware
     {
         void OnNavigatedTo(object parameter);
