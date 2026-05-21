4f38300f038807f9bdd4424b89f3aa7d9976fe4f
klochkovdy-creator <klochkovdy@mer.ci.nsu.ru>
2026-05-21 11:56:15 +0700

    Update IContactService.cs

--- Services/IContactService.cs
+++ Services/IContactService.cs
@@ -1,14 +1,13 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using PhoneBook.Models;
 
 namespace PhoneBook.Services
 {
-    // Контракт сервиса работы с контактами.
-    // Абстракция позволяет легко заменить хранилище (in-memory → база данных)
-    // без изменения ViewModels.
     public interface IContactService
     {
         List<Contact> GetAllContacts();
         void UpdateContact(Contact contact);
+        void AddContact(Contact contact);
+        void DeleteContact(int id);
     }
 }
