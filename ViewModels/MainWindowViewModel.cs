b6e059c6f7158721cff9aabb5cfc6befcfc8e571
klochkovdy-creator <klochkovdy@mer.ci.nsu.ru>
2026-05-21 11:59:30 +0700

    Update MainWindowViewModel.cs

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -3,14 +3,11 @@ using PhoneBook.Services;
 
 namespace PhoneBook.ViewModels
 {
-    // ViewModel главного окна-оболочки (Shell).
-    // Управляет навигацией между экранами через команды меню.
+
     public class MainWindowViewModel : ObservableObject
     {
         private readonly INavigationService _navigation;
 
-        // Публичное свойство для привязки ContentControl в XAML:
-        // Content="{Binding NavigationService.CurrentViewModel}"
         public INavigationService NavigationService => _navigation;
 
         public ICommand ShowContactsCommand { get; }
@@ -20,10 +17,8 @@ namespace PhoneBook.ViewModels
         {
             _navigation = navigation;
 
-            // При запуске сразу открываем экран списка контактов
             _navigation.NavigateTo<ContactsListViewModel>();
 
-            // Команды меню — переключают активный экран через NavigationService
             ShowContactsCommand = new RelayCommand(_ => _navigation.NavigateTo<ContactsListViewModel>());
             ShowAboutCommand = new RelayCommand(_ => _navigation.NavigateTo<AboutViewModel>());
         }
